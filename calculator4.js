3d34079e2ca3134042f0d4bbe1ef6e685ce00b06
DESKTOP-4N4R3LH\jqk17 <jqk1797@gmail.com>
2024-11-23 02:43:45 +0900

    입력된 값의 타입에 따른 유효성 검사 메서드 구현

--- calculator4.js
+++ calculator4.js
@@ -1,5 +1,5 @@
 // 입력 처리를 담당하는 클래스
-class InputHandler {  
+class InputHandler {
     #buttonInput;
     #keyboardInput;
     #input;
@@ -9,7 +9,7 @@ class InputHandler {
         this.handleKeyboardInput();
         this.#input = '';
     }
-    
+
     // '버튼 클릭 입력' 처리 메서드
     handleButtonInput() {
         const buttonElements = document.querySelectorAll('.btn');
@@ -32,20 +32,86 @@ class InputHandler {
 
     }
 
-    // '모든 입력' 처리 메서드
+    // value 입력 처리 메서드
     handleInput(value) {
-        if (this.inputValidation(this.#input, value)) {
-            this.#input += `${value}`;
+        if (value === 'Clear') {
+            this.#input = '';
+            console.log('Clear');
+            return;
+        } else if (value === 'Backspace') {
+            this.#input = this.#input.slice(0, -1);
+            console.log('input입력값:', this.#input);
+            return;
+        }
+        // 입력 유효성 검사
+        if (this.inputValidation(value)) {
+            // 입력 추가
+            this.#input += value;
+            console.log('input입력값:', this.#input);
+        } else {
+            console.log('미완성 수식');
         };
-        console.log('입력받은 값:', this.#input, value);
     }
 
     // 입력 유효성 검사 메서드
-    inputValidation(input, value) {
+    inputValidation(value) {
+        // 입력값 타입 확인
         const lastInput = this.#input.slice(-1);
-        switch (lastInput, value) {
-            case '/[]/':
+        const typeOfLastInput = this.isWhatType(lastInput);
+
+        // 입력값 타입에 따른 유효성 검사
+        switch (this.isWhatType(value)) {
+            case 'number':
                 return true;
+            case 'operator':
+                if (typeOfLastInput === 'operator') {
+                    this.#input = this.#input.slice(0, -1);
+                } else if (typeOfLastInput === 'leftParentheses') {
+                    return false;
+                } else if (this.#input === '') {
+                    return false;
+                }
+                return true;
+            case 'leftParentheses':
+                if (typeOfLastInput === 'number' || typeOfLastInput === 'rightParentheses') {
+                    this.#input += '*';
+                }
+                return true;
+            case 'rightParentheses':
+                const openCount = this.#input.match(/\(/g) || [];
+                const closeCount = this.#input.match(/\)/g) || [];
+                if (openCount.length <= closeCount.length) {
+                    return false;
+                }
+                if (typeOfLastInput === 'operator' || typeOfLastInput === 'leftParentheses') {
+                    return false;
+                }
+                return true;
+            case 'equal':
+                if (typeOfLastInput === 'operator' || typeOfLastInput === 'leftParentheses') {
+                    return false;
+                }
+                return true;
+        }
+    }
+
+    isWhatType(value) {
+        // 정규표현식
+        if (/[0-9]/.test(value)) {
+            return 'number';
+        }
+        switch (value) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                return 'operator';
+            case '(':
+                return 'leftParentheses';
+            case ')':
+                return 'rightParentheses';
+            case '=':
+                return 'equal';
         }
     }
 }
@@ -60,15 +126,15 @@ class Display {
 }
 
 // 계산 로직을 담당하는 클래스
-class Calculator {}
+class Calculator { }
 
-class OperatorStrategy {}
+class OperatorStrategy { }
 
-class NumberStrategy {}
+class NumberStrategy { }
 
-class UpdateDisplayState {}
+class UpdateDisplayState { }
 
-class CalculatorState {}
+class CalculatorState { }
 
 
 // 인스턴스 생성
